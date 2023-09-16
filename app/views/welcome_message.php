9cedc48cd9cf55eceb5005f87aaeebcb6ea459a7
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-09-16 10:06:26 -0500

    phpversion() en welcome

--- app/views/welcome_message.php
+++ app/views/welcome_message.php
@@ -94,6 +94,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">PHP version <?php echo  phpversion() ?></p>
 </div>
 
 </body>
